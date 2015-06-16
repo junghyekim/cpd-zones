6094e999c72fae3d66921ef305d6e45d1eedf636
Sean Brewer <seabre986@gmail.com>
2015-06-16 13:27:06 -0400

    Do error handling. Add flash. Modify officer images.

--- source/javascripts/app/map.js
+++ source/javascripts/app/map.js
@@ -12,6 +12,10 @@ function render_template(v) {
   $('.zone-results').append(output);
 }
 
+function flash_message(type, msg) {
+  return "<div class='flash-" + type + "'><span>" + msg + "</span></div>";
+}
+
 L.Icon.Default.imagePath = 'images/leaflet-img';
 
 var map = L.map('map', {
@@ -43,24 +47,40 @@ omnivore.topojson('data/CPDZones.topojson')
 
       // Clear result div
       $('.zone-results').html('');
+      // Clear flash
+      $('#flash').html('');
 
       L.esri.Geocoding.Tasks.geocode().text(input).run(function(err, results, response){
-        var result = results.results[0];
-        var inpolygon_result = leafletPip.pointInLayer(result.latlng, zones)[0];
-        var inpolygon_props = inpolygon_result.feature.properties;
+        if (err) {
+          $('#flash').html(flash_message('error', 'There was an issue finding where you live.'));
+        } else {
+          if (results.results.length > 0) {
+            var result = results.results[0];
+            var inpolygon_results = leafletPip.pointInLayer(result.latlng, zones);
 
-        marker = new L.Marker(result.latlng);
-        map.addLayer(marker);
-        map.setView(result.latlng);
-        console.log(inpolygon_result.feature.properties);
-        render_template({
-          image: inpolygon_props.CAPT_IMG,
-          name: inpolygon_props.CAPT,
-          email: inpolygon_props.CAPT_EMAIL});
-        render_template({
-          image: inpolygon_props.LT_IMG,
-          name: inpolygon_props.LT,
-          email: inpolygon_props.LT_EMAIL});
+            if (inpolygon_results.length > 0) {
+              var inpolygon_result = inpolygon_results[0];
+              var inpolygon_props = inpolygon_result.feature.properties;
+
+              marker = new L.Marker(result.latlng);
+              map.addLayer(marker);
+              map.setView(result.latlng);
+              console.log(inpolygon_result.feature.properties);
+              render_template({
+                image: inpolygon_props.CAPT_IMG,
+                name: inpolygon_props.CAPT,
+                email: inpolygon_props.CAPT_EMAIL});
+              render_template({
+                image: inpolygon_props.LT_IMG,
+                name: inpolygon_props.LT,
+                email: inpolygon_props.LT_EMAIL});
+            } else {
+              $('#flash').html(flash_message('error', "Your address wasn't found in a region. Try another address."));
+            }
+          } else {
+            $('#flash').html(flash_message('error', 'No results found for your address.'));
+          }
+        }
       });
     });
   })
