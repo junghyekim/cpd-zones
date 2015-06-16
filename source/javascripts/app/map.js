23af5a1e243b49f23bac430e4195dc6aa50f1288
Sean Brewer <seabre986@gmail.com>
2015-06-16 11:39:00 -0400

    Add officer images. Update topojson data with e-mail addresses. Add card template.

--- source/javascripts/app/map.js
+++ source/javascripts/app/map.js
@@ -2,9 +2,15 @@ var southWest = L.latLng(34.9816, -85.4719);
 var northEast = L.latLng(35.217, -85.0462);
 var center = L.latLng(35.0657, -85.241);
 var bounds = L.latLngBounds(southWest, northEast);
-var tiles = '//otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png'
+var tiles = '//otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
+var card_template = $('#card_template').html();
 var marker;
 var zones;
+
+function render_template(v) {
+  var output = Mustache.render(card_template, v);
+  $('.zone-results').append(output);
+}
 
 L.Icon.Default.imagePath = 'images/leaflet-img';
 
@@ -28,17 +34,33 @@ omnivore.topojson('data/CPDZones.topojson')
     });
 
     $('input[value="Locate"]').click(function() {
+
       if (marker !== undefined) {
         map.removeLayer(marker);
       }
+
       var input = $('#query').val();
+
+      // Clear result div
+      $('.zone-results').html('');
+
       L.esri.Geocoding.Tasks.geocode().text(input).run(function(err, results, response){
         var result = results.results[0];
-        var inpolygon_result = leafletPip.pointInLayer(result.latlng, zones);
+        var inpolygon_result = leafletPip.pointInLayer(result.latlng, zones)[0];
+        var inpolygon_props = inpolygon_result.feature.properties;
 
         marker = new L.Marker(result.latlng);
         map.addLayer(marker);
         map.setView(result.latlng);
+        console.log(inpolygon_result.feature.properties);
+        render_template({
+          image: inpolygon_props.CAPT_IMG,
+          name: inpolygon_props.CAPT,
+          email: inpolygon_props.CAPT_EMAIL});
+        render_template({
+          image: inpolygon_props.LT_IMG,
+          name: inpolygon_props.LT,
+          email: inpolygon_props.LT_EMAIL});
       });
     });
   })
--- source/javascripts/vendor.js
+++ source/javascripts/vendor.js
@@ -1,4 +1,5 @@
 //= require 'vendor/jquery/jquery-1.11.3'
+//= require 'vendor/mustache/mustache'
 //= require 'vendor/leaflet/leaflet-src'
 //= require 'vendor/omnivore/leaflet-omnivore'
 //= require 'vendor/esri-leaflet/esri-leaflet-src'
